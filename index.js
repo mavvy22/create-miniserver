ce095baf50d8d7fa0c43941774d931f4cdda5734
ESPIM146 <Jeric.Espiritu.-ND@disney.com>
2024-01-05 04:22:19 +0800

    fix: use bin

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+#!/usr/bin/env node
+
 import fs from 'fs-extra';
 
 function main() {
+  const argv = process.argv;
+  console.log(argv);
   console.log('creating template');
 
   fs.copySync('./template', process.cwd());
